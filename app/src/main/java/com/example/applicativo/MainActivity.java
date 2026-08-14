d0f2e906eb4aecb4e6eba01e0b978cfba904fbd2
Colly <colly@gmail.com>
2026-08-14 12:11:42 -0300

    botão clicante

--- app/src/main/java/com/example/applicativo/MainActivity.java
+++ app/src/main/java/com/example/applicativo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.applicativo;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +12,19 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    int contador=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        Button b = findViewById(R.id.button);
+        TextView tv = findViewById(R.id.textView);
+        b.setOnClickListener(view -> {
+            contador++;
+            tv.setText(Integer.toString(contador));
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
